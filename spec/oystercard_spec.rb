1f977371f3d86fa34b74582ab7ebf927e866923f
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-30 09:41:01 +0000

    Challenge #04: Oystercard now has a balance of 0 when initialized, also responds to .balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
 
+  context 'balance' do
+
+    it { is_expected.to respond_to(:balance) }
+
+    it 'checks if card balance is 0' do
+      expect(subject.balance).to eq 0
+    end
+
+  end
+  
 end
